2079a61fc72a058a990b6d7ffbe50cc4142a70ec
Gyula Szabó <gyufi@szabocsalad.com>
2020-12-15 14:08:18 +0100

    add some config validation

--- www/testcollector.php
+++ www/testcollector.php
@@ -5,18 +5,31 @@
 require_once('_include.php');
 
 SimpleSAML\Utils\Auth::requireAdmin();
-
 SimpleSAML\Logger::info('SAML2.0 - AttributeCollector module test endpoint');
 
-
 $config = SimpleSAML\Configuration::getInstance();
-$configauthproc = $config->getArray('authproc.aa', NULL);
+$configauthprocs = [
+    $config->getArray('authproc.aa', NULL);
+    $config->getArray('authproc.sp', NULL);
+    $config->getArray('authproc.idp', NULL);
+    ];
 
-foreach($configauthproc as $filter) {
-  if ($filter['class'] == 'attributecollector:AttributeCollector') {
-    $collectorFilterConfig = $filter['collector'];
-    break;
+// test only the first collector in authproces
+foreach ($configauthprocs as $configauthproc) { 
+  foreach($configauthproc as $filter) {
+    if ($filter['class'] == 'attributecollector:AttributeCollector') {
+      $collectorFilterConfig = $filter['collector'];
+      break 2;
+    }
   }
+}
+
+if (!$collectorFilterConfig) {
+  throw new SimpleSAML\Error\Exception('There is no collector in authproces.');
+}
+
+if ($collectorFilterConfig['class'] != 'attributecollector:SQLCollector') {
+  throw new SimpleSAML\Error\Exception('The tester works with only sqlcollector. Sorry.');
 }
 
 $sqlCollector = new sspmod_attributecollector_Collector_SQLCollector($collectorFilterConfig);
@@ -24,14 +37,14 @@ $sqlCollector = new sspmod_attributecollector_Collector_SQLCollector($collectorF
 $uid = $_POST['uid'];
 
 if ($uid && !preg_match('/^[a-zA-Z0-9_]*$/',$uid)) {
- throw new Exception('UID is not valid');
+ throw new SimpleSAML\Error\Exception('UID is not valid');
 }
 
 $result = $sqlCollector->getAttributes(['AatId' => [$uid]], 'AatId');
 
 $attributes = $result;
 
-$t = new \SimpleSAML\XHTML\Template($config, 'attributecollector:status.php', 'attributes');
+$t = new SimpleSAML\XHTML\Template($config, 'attributecollector:status.php', 'attributes');
 
 $t->data['query'] = $collectorFilterConfig['query'];
 $t->data['attributes'] = $attributes;
